3e7de4b24fb5044251f9d529211435a0a5727c80
Nordine000 <ounissi970@gmail.com>
2025-01-17 21:57:49 +0000

    All scripts loops python

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("".join("{}".format(chr(letter)) for letter in range(97, 123) if letter not in (101, 113)), end="")
+for alpha in range(97, 123):
+    if alpha != 101 and alpha != 113:
+        print("{:c}".format(alpha), end='')
