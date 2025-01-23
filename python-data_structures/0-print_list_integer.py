185f52fdfdaebcb9dbfca9b74afe9e1278a066b5
Nordine000 <ounissi970@gmail.com>
2025-01-23 00:04:49 +0000

    task 5

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for numbre in my_list:
-        print("{:d}".strformat(number))
+    for number in my_list:
+        print("{:d}".format(number))
---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    po = ''
+    for i in my_string:
+        if i != 'C' and i != 'c'
+        po += i
+
+        return po
