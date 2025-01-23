52c0dbc639055bdcb78a6a29c517b468c6bb13a0
Nordine000 <ounissi970@gmail.com>
2025-01-23 00:30:39 +0000

    probleme task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    po = ''
-    for i in my_string:
-        if i not in ('C','c'):
-        po += i
+    new_str = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_str += char
 
-        return po
+        return new_str
