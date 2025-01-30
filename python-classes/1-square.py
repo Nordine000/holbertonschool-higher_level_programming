a258a2d9126134a5c40c6b6fdbb065a137a02286
Nordine000 <ounissi970@gmail.com>
2025-01-30 12:55:36 +0000

    defines a square

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Une classe utilisée pour représenter un caé
+"""
+
+
+
+class Square:
+    """
+    class defined for square generation
+    """
+    pass
+
+def my_square(self, size):
+   self.__size = size
