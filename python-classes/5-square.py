362f8e545faef44a44a62fcc043a72f73662304c
Nordine000 <ounissi970@gmail.com>
2025-01-31 19:42:42 +0000

    The oop

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -14,10 +14,10 @@ class Square:
     """class defined for square generation
     """
     def __init__(self, size=0):
-        self.__size = size
-
-    @property
+        self.size = size
+ @property
     def size(self):
+        """Gets the size of the square."""
         return self.__size
 
     @size.setter
@@ -33,8 +33,7 @@ class Square:
 
     def my_print(self):
         if self.__size == 0:
-            print()
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print('#', end='')
-            print()
+            print("")
+            return
+        for _ in range(self.__size):
+            print("#" * self.__size
