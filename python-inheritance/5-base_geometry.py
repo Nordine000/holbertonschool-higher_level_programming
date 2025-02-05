02a5f9629ec63309d685ea3d1f9407c9f1aff965
Nordine000 <ounissi970@gmail.com>
2025-02-05 11:32:45 +0000

    Classe vide basegeometry

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 Class vide pour task 5 et autres
 """
@@ -15,8 +14,8 @@ class BaseGeometry:
     Creation d'objet
     """
 
-    bg = BaseGeometry()
+bg = BaseGeometry()
 
-    print(bg)
-    print(dir(bg))
-    print(dir(BaseGeometry))
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
