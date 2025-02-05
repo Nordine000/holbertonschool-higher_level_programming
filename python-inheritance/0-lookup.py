8c5ee71c1815fa18db190c9bba10d57ff77cff45
Nordine000 <ounissi970@gmail.com>
2025-02-05 11:37:22 +0000

    The lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-class MyClass1(object):
-    pass
+"""
+Module that defines the lookup function.
+"""
 
-class MyClass2(object):
-    my_attr1 = 3
-    def my_meth(self):
-        pass
+
 def lookup(obj):
     """Returns the list of available attributes and methods of an object."""
     return dir(list)
