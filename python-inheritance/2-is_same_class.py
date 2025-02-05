22d6c847a718399b8f2085a289ff87e1dc82ef8e
Nordine000 <ounissi970@gmail.com>
2025-02-05 13:50:04 +0000

    Task 2 : verifivation objet

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,20 +4,18 @@ verifie si l'objet est une instance de class
 """
 
 
-
 def is_same_class(obj, a_class):
     """
-    Retourne True si l'objet est exactement une instance de la classe spécifiée,
-    sinon False.
+    Retourne True si l'objet est exactement une instance de la classe spécifie,
+    sinon False
 
     Parameters:
-    obj : object
-        L'objet vérifier.
+    obj : object L'objet vérifir
     a_class : type
-        La classe comparer avec le type de l'objet.
-
+        La classe comparer avec le type de l'objet
     Returns:
     bool
-        True si l'objet est exactement une instance de a_class, sinon False.
-    """
+    True si l'objet est exactement une instance de a_class,
+        sinon False
+        """
     return type(obj) is a_class
