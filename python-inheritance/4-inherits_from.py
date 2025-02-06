c390ca7516d37063202bca630e50661dc9c723ce
Nordine000 <ounissi970@gmail.com>
2025-02-06 00:32:39 +0000

    Betty c'etait mieux

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Verifie si l'objet est une instance de classe
+qui a heriter directement ou indirectement de la classe specifier
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Return True i l'objet est une instance de classe qui a heriter
+    directement ou indirectement de la classe spécifie
+    sinon False
+    """
+    return issubclass(obj, a_class)
