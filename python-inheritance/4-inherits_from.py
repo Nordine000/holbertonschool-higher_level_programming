a69caf496d2bea7d1b4290e777b1010b6118af85
Nordine000 <ounissi970@gmail.com>
2025-02-06 00:35:57 +0000

    Betty c'etait mieux

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -11,4 +11,4 @@ def inherits_from(obj, a_class):
     directement ou indirectement de la classe spécifie
     sinon False
     """
-    return isinstance(obj, a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
