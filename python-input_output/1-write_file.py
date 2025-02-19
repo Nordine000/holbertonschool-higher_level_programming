13fb3a3320e4e37551955a30ab382067dbc53c6c
Nordine000 <ounissi970@gmail.com>
2025-02-19 13:58:48 +0000

    Project Input-output

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+'''
+This module contains one function write_file
+'''
+
 
 def write_file(filename="", text=""):
-     """»Ouvrir et Ã©crire du texte dans un fhier"""
-    with open(filename, 'w', encoding="utf-8") as f:
+    """Open and write text in file"""
+    with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
-        return len(text)
+    return len(text)
