7c85f816272dbe3cfe09cbd0066eaa2caff27c67
Nordine000 <ounissi970@gmail.com>
2025-02-19 13:55:04 +0000

    Project Input-output

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
 
-
+def write_file(filename="", text=""):
+     """»Ouvrir et Ã©crire du texte dans un fhier"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(text)
+        return len(text)
