36f3ce68cf032ead72efe7831c109fa1b848577e
Nordine000 <ounissi970@gmail.com>
2025-02-19 13:43:07 +0000

    Project Input-output

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     Lit un fichier texte (UTF8) et l'affiche sur stdout
     """
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
