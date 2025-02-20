de1be90c59ea68225c61ae98d1365c15ee9edaba
Nordine000 <ounissi970@gmail.com>
2025-02-20 11:23:40 +0000

    Module de serialisation et deserialisation

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+basic serialization module that adds the functionality
+to serialize a Python dictionary to a JSON file and
+deserialize the JSON file to recreate the Python Dictionary.
+"""
+import pickle
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Sérialise les données et les sauvegarde dans unichier
+    """
+    with open(filename, 'wb') as fichier:
+        pickle.dump(data, fichier)
+    pass
+
+
+def load_and_deserialize(filename):
+    """
+    desérialise les données et les sauvegarde da un fichi
+    """
+    with open(filename, "rb") as f:
+        deserialized_dict = pickle.load(f)
+    pass
