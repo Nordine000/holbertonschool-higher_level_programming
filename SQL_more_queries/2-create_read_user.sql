c9398bd50fa95e72739c6d8147671a793e82a35f
root <Nordine000>
2025-03-06 11:04:53 +0000

    SQL 2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- cree une base de donn√es, si elle existe elle doit pas echouer
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- crre un utilisateur avec sont mot de passe, si il existe il ne doit pas echouer
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,2 @@
+-- crée la table force_nameavec valeur donc cgar ne peut pas etre nul
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
