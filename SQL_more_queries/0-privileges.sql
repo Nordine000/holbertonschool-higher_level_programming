2a1c6b2341d12a6b10423713cba4c137e314f66b
root <Nordine000>
2025-03-06 08:54:15 +0000

    SQL partie 2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Liste les privile¨gs de l'utilisateur user
--- Liste les privile¨g de l 'utilisateur user2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Liste les privileg de l 'utilisateur user2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
