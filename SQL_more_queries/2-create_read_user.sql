7fe2d7a794796442c02967d0fb50efe029d3ddfc
root <Nordine000>
2025-03-06 11:11:47 +0000

    SQL 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,3 +2,5 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- crre un utilisateur avec sont mot de passe, si il existe il ne doit pas echouer
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- accored tt les privilege a l'user
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
