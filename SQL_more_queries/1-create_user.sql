190a69ecba7f96c04759d58c4e3e4e32157aa4c5
root <Nordine000>
2025-03-06 09:57:09 +0000

    SQL 2

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- cree un utilisateur avec sont mpt de passe
-CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- on lui donne tout les acces/privileges
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
