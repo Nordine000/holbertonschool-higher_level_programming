01ebc9e592e20029d7502f4f65752eab6b906b47
Nordine000 <ounissi970@gmail.com>
2025-03-07 22:39:35 +0000

    Python

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,24 +1,39 @@
 #!/usr/bin/python3
-"""adds the State object “Louisiana”
-to the database hbtn_0e_6_usa"""
+"""List all City objects from the database hbtn_0e_14_usa"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+from model_city import City  # N'oubliez pas d'importer la classe City !
 
 if __name__ == "__main__":
 
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
+    # Vérification du nombre d'arguments
+    if len(sys.argv) != 4:
+        exit(1)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    # Connexion à la base de données
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-    session = Session(engine)
-    new = State(name='Louisiana')
-    session.add(new)
-    new_state = session.query(State).filter(State.name == 'Louisiana').first()
-    session.commit()
-    print("{}".format(new_state.id))
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(username, password, db_name),
+        pool_pre_ping=True
+    )
+
+    # Création de la session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Interroger les villes et les états associés
+    cities = session.query(City, State).join(State).order_by(City.id).all()
+
+    # Affichage des résultats
+    for city, state in cities:
+        print(f"{state.name}: ({city.id}) {city.name}")
+
+    # Fermeture de la session
     session.close()
--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
-'''
-prend un argument
-et affiche toutes les valeurs dans la table des états de hbtn_0e_0_us
-où le nom correspon l'argument
-'''
+""" List all states with a name starting with N from the hbtn_0e_0_usa """
+
 import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
+def list_states():
 
-    if len(sys.argv) != 5:
-        exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+    # Retrieving arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-            user=username, passwd=password, db=db_name)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states "
-            "WHERE name = '{}' "
-            "ORDER BY id ASC".format(state_name))
-    states = cursor.fetchall()
+    try:
+        # Connect to the MySQL database
+        db = MySQLdb.connect(host="localhost", user=mysql_username,
+                        passwd=mysql_password, db=database_name, port=3306)
 
-    for state in states:
-        print(state)
+        cursor = db.cursor()
 
-    cursor.close()
-    db.close()
+        # Execute SQL query
+        cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name))
+
+        # Fetch all results
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print(f"SQL connection or execution error : {e}")
+
+    finally:
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
+
+if __name__ == "__main__":
+    list_states()
