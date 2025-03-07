b925d72af391793c872c80b52a5a6fc00f136ebf
Nordine000 <ounissi970@gmail.com>
2025-03-07 20:27:55 +0000

    SQL

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- repertorie emissions comique
+SELECT s.title
+FROM tv_shows s
+JOIN tv_show_genres sg ON s.id = sg.show_id
+JOIN tv_genres g ON sg.genre_id = g.id
+WHERE g.name = 'Comedy'
+ORDER BY s.title ASC;
