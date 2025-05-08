74dd329b96ae85dbe79e2aa1b5f27bb5b13fdc32
Nordine000 <ounissi970@gmail.com>
2025-05-09 01:20:15 +0200

    imprime une chaine avec un debut et une fin

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[str.find("object-oriented programming"):
-          str.find("object-oriented programming") +
-          len("object-oriented programming")] + " with Python")
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
