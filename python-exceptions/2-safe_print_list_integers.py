afc8db89ee7c104c419658081e8db11808ed3330
Nordine000 <ounissi970@gmail.com>
2025-05-15 23:48:05 +0200

    change break/pass avec raise qui declenche une exception

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,6 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
                 print(my_list[i], end="")
                 n += 1
         except IndexError:
-            pass
+            raise
     print()
     return n
