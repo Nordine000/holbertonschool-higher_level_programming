cb610dc5c85e768c828dfe52de6a9f479abf143d
Nordine000 <ounissi970@gmail.com>
2025-05-16 14:04:39 +0200

    utilisation de finally pour un clacul

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    result = None
     try:
         result = a /b
-       # print("{}".format(result))
-    except (ZeroDivisionError):
+    except ZeroDivisionError:
         result = None
     finally:
         print("Inside Result: {}".format(result))
-        return result
+    return result
