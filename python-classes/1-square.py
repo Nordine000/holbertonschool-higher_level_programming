9990bcef3ea4a798aa6d83fc23353f7fd3d76feb
Nordine000 <ounissi970@gmail.com>
2025-05-21 15:38:07 +0200

    classe carrer avec un attribut prive size

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+classe carrer avec attribut priver sizz
+"""
+
+
+class Square():
+    """ la classe qui definis carrer avec un attribut privez"""
+    def __init__(self, size):
+        """Initialise un carré avec une taille privée."""
+        pass
