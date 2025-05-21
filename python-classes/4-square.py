f4b14655d55af92ebaa7a0b0a4e39643056e8f40
Nordine000 <ounissi970@gmail.com>
2025-05-21 17:09:14 +0200

    return la surface au carre

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+classe carrer avec attribut priver size avec validation de taille
+"""
+
+
+class Square():
+    """ la classe qui definis carrer avec un attribut privez"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
