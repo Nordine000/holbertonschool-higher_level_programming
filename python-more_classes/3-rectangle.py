e6fa2d93732557a6040fec2102f75d1a5a30af09
Nordine000 <ounissi970@gmail.com>
2025-05-23 14:36:13 +0200

    test

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -45,9 +45,9 @@ class Rectangle():
 
     def perimeter(self):
         """Retourne la perimetre du rectangle"""
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.width + self.height)
+        return 2 * (self.__width + self.__height)
 
     def __str__(self):
         """Renvoie chaîne de caractères du rectangle en utilisant #"""
