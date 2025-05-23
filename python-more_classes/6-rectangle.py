db1068b2a66c030724249f8f1d0ecbe6e9658b8c
Nordine000 <ounissi970@gmail.com>
2025-05-23 23:13:52 +0200

    test

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -65,3 +65,4 @@ class Rectangle():
     def __del__(self):
         """imprime une meessage lorsqu'une instance de rectangle est sup"""
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
