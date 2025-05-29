c577b063cce0f9372dfb7946862202bc1aac7a63
Nordine000 <ounissi970@gmail.com>
2025-05-30 01:16:33 +0200

    test

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,6 +5,4 @@ Fonction qui renvoie true si l'obj est une instance de classe spe sinon false
 
 
 def is_same_class(obj, a_class):
-    if isinstance(obj, a_class):
-        return True
-    return False
+    return type(obj) is a_class
