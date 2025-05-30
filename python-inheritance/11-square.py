8ff8a6cd15fbdef455349472218dde64a44f7ca4
Nordine000 <ounissi970@gmail.com>
2025-05-30 17:22:01 +0200

    test

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -25,7 +25,7 @@ class Square(Rectangle):
         super().__init__(size, size)
 
         def __str__(self):
-            return f"[Square] {self.__width}/{self.__height}"
+            return f"[Square] {self.__size}/{self.__size}"
 
     def area(self):
         """
