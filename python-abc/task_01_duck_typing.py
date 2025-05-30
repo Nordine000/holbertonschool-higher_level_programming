f7004ac0592532bb87e0c8cae3c98be414d3268b
Nordine000 <ounissi970@gmail.com>
2025-05-30 23:49:42 +0200

    test

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -24,15 +24,18 @@ class Circle(Shape):
     def perimeter(self):
         return 2 * math.pi * abs(self.radius)
 
+
 class Rectangle(Shape):
-     
+    """
+    Classe Rectangle héritant de Shape
+    """
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        
+
     def area(self):
         return self.width * self.height
-        
+
     def perimeter(self):
         return 2 * (self.width + self.height)
 
