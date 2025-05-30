40f0ab54ef9160452104b5258fb100aba3b75591
Nordine000 <ounissi970@gmail.com>
2025-05-30 16:14:44 +0200

    test

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Fonction de creation de la classe Square héritant de Rectangle.
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+"""
+import de rectangle dans le fichier 9
+"""
+
+
+class Square(Rectangle):
+
+    def __init__(self, size):
+        """
+        Initialise une instance Square.
+
+        Arguments:
+        size (int): taille du carré.
+        Size doit être un entier positif, validé par integer_validator.
+
+        Super() fait en sorte que sqaure herite biend des attributs rectangle
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calcule l'aire du carré.
+        """
+        return self.__size * self.__size
