81e4a0fe6e172e2f1be455a4da4785223190eaa6
Nordine000 <ounissi970@gmail.com>
2025-06-04 12:40:48 +0200

    ecrire dnas un fichier

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" def qui ecrit une chaine dans un fcihier texte et return le nbre de char ecrit"""
+def write_file(filename="", text=""):
+    """
+    ouvre un fichier et ecris a l'interieur
+    et reurn le nbre de char
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
