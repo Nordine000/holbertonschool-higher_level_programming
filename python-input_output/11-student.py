6f9cc62cb818fadb9dc85e0eebb5c271a797bae5
Nordine000 <ounissi970@gmail.com>
2025-06-06 22:08:27 +0200

    test

---
+++ python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Definit une classe Student.
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initialise une nouvelle instance student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(attr, str)
+                                           for attr in attrs):
+            return {key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
