2d7b8a1bd741f728c0c7b5268a98d0bdbe18b80b
Nordine000 <ounissi970@gmail.com>
2025-06-06 02:33:27 +0200

    test

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+ajout d'arg a une liste python et enregistre dans un fcihier
+"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+file = "add_item.json"
+
+try:
+    listes = load_from_json_file(file)
+except FileNotFoundError:
+    listes = []
+
+    listes.extend(sys.argv[1:])
+    save_to_json_file(listes, file)
