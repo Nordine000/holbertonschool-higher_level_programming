fcb3bdc38c4bd9f9856e08c0d7a61ba08ce2c86a
Nordine000 <ounissi970@gmail.com>
2025-06-27 01:17:51 +0200

    task

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Supprime la base de données hbtn_0c_0 si elle existe
+-- DELETE sert à supprimer des données dans une table, pas des structures entières comme des bases ou des tables)
+DROP DATABASE IF EXISTS hbtn_0c_0;
